84a6e585e16e88b7ada18dd90bb09548fa8a88f0
bunnyhoops123 <sumukh.varmay@gmail.com>
2023-09-08 17:31:33 +0530

    chapter 4

--- chapter03/index.js
+++ chapter03/index.js
@@ -1,1 +1,6 @@
-console.log('testing');
+const format = require('date-fns').format;
+const { v4: uuid } = require('uuid'); //import Version 4 as uuid
+
+console.log(format(new Date(), 'yyyyMMdd\tHH:mm:ss'));
+
+console.log(uuid());
